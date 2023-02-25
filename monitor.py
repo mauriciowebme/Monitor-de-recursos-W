1ef617febfe489a62595a324002d231b73477232
Mauricio Azeredo <mauriciowebme@gmail.com>
2023-02-25 09:56:51 -0300

    testes

--- monitor.py
+++ monitor.py
@@ -1,111 +1,20 @@
-import platform,socket,psutil,time
-from tkinter import *
-from tkinter import ttk
-
-class Application:
-    def __init__(self, master=None):
-        self.widget1 = Frame(master)
-        self.widget1.pack()
-        self.msg = Label(self.widget1, text="Primeiro widget")
-        self.msg["font"] = ("Calibri", "9", "italic")
-        self.msg.pack ()
-        self.sair = Button(self.widget1)
-        self.sair["text"] = "Clique aqui"
-        self.sair["font"] = ("Calibri", "9")
-        self.sair["width"] = 50
-        self.sair.bind("<Button-1>", self.mudarTexto)
-        self.sair.pack ()
-
-    def mudarTexto(self, event):
-        if self.msg["text"] == "Primeiro widget":
-            self.msg["text"] = "O botão recebeu um clique"
-        else:
-            self.msg["text"] = "Primeiro widget"
+import time
+import wmi
+import clr
 
 
-root = Tk()
-Application(root)
-root.mainloop()
- 
-def verificarSistema():
-    info={}
-    info['platform']=platform.system()
-    info['platform-release']=platform.release()
-    info['platform-version']=platform.version()
-    info['architecture']=platform.machine()
-    info['hostname']=socket.gethostname()
-    info['processor']=platform.processor()
-    info['ram']=str(round(psutil.virtual_memory().total / (1024.0 **3)))
-    
-    print(info)
-    while True:
-        print(psutil.cpu_percent())
-        time.sleep(1)
+file = r'C:\DESENVOLVIMENTO\Monitor de recursos W\OpenHardwareMonitor\OpenHardwareMonitorLib.dll'
+clr.AddReference(file)
 
-import clr #package pythonnet, not clr
+w = wmi.WMI(namespace = r"root\OpenHardwareMonitor")
 
-
-openhardwaremonitor_hwtypes = ['Mainboard','SuperIO','CPU','RAM','GpuNvidia','GpuAti','TBalancer','Heatmaster','HDD']
-cputhermometer_hwtypes = ['Mainboard','SuperIO','CPU','GpuNvidia','GpuAti','TBalancer','Heatmaster','HDD']
-openhardwaremonitor_sensortypes = ['Voltage','Clock','Temperature','Load','Fan','Flow','Control','Level','Factor','Power','Data','SmallData']
-cputhermometer_sensortypes = ['Voltage','Clock','Temperature','Load','Fan','Flow','Control','Level']
-
-
-def initialize_openhardwaremonitor():
-    file = 'OpenHardwareMonitorLib.dll'
-    clr.AddReference(file)
-
-    from OpenHardwareMonitor import Hardware
-
-    handle = Hardware.Computer()
-    handle.MainboardEnabled = True
-    handle.CPUEnabled = True
-    handle.RAMEnabled = True
-    handle.GPUEnabled = True
-    handle.HDDEnabled = True
-    handle.Open()
-    return handle
-
-def initialize_cputhermometer():
-    file = 'CPUThermometerLib.dll'
-    clr.AddReference(file)
-
-    from CPUThermometer import Hardware
-    handle = Hardware.Computer()
-    handle.CPUEnabled = True
-    handle.Open()
-    return handle
-
-def fetch_stats(handle):
-    for i in handle.Hardware:
-        i.Update()
-        for sensor in i.Sensors:
-            parse_sensor(sensor)
-        for j in i.SubHardware:
-            j.Update()
-            for subsensor in j.Sensors:
-                parse_sensor(subsensor)
-
-def parse_sensor(sensor):
-        if sensor.Value is not None:
-            if type(sensor).__module__ == 'CPUThermometer.Hardware':
-                sensortypes = cputhermometer_sensortypes
-                hardwaretypes = cputhermometer_hwtypes
-            elif type(sensor).__module__ == 'OpenHardwareMonitor.Hardware':
-                sensortypes = openhardwaremonitor_sensortypes
-                hardwaretypes = openhardwaremonitor_hwtypes
-            else:
-                return
-
-            if sensor.SensorType == sensortypes.index('Temperature'):
-                print(u"%s %s Temperature Sensor #%i %s - %s\u00B0C" % (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
-
-if __name__ == "__main__":
-    print("OpenHardwareMonitor:")
-    HardwareHandle = initialize_openhardwaremonitor()
-    fetch_stats(HardwareHandle)
-    print("\nCPUMonitor:")
-    CPUHandle = initialize_cputhermometer()
-    fetch_stats(CPUHandle)
-    verificarSistema()
-    o=0
+teste = w.classes
+while True:
+    temperature_infos = w.Sensor()
+    if len(temperature_infos) > 0:
+        break
+    time.sleep(1)
+for sensor in temperature_infos:
+    if sensor.SensorType==u'Temperature':
+        print(sensor.Name)
+        print(sensor.Value)
